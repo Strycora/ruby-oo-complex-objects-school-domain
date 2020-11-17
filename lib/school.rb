4fdfed86168ed4648a574ebe3d85e8a643313144
Vikram Chatterjee <strycora@gmail.com>
2020-11-17 15:30:25 -0700

    commit tree 049b28c4066712c5f134246ccc3368a86b81a406

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require "pry"
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def add_student(name, grade)
+        if @roster.include?(grade)
+            @roster[grade]<< (name) 
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def sort
+        sort_roster = {}
+        @roster.collect do |grade, names|
+            sort_roster[grade]=names.sort
+        end
+        sort_roster
+    end
+
+
+end
